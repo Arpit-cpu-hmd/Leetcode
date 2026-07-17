85a60ff59bf379af1d49c5df7c2dcb0735dc173d
Arpit Titoria <arpittitoria12@gmail.com>
2026-07-17 22:40:29 +0530

    Time: 0 ms (100%), Space: 8.9 MB (38.2%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>str;
+
+        for(int i=0; i< s.size(); i++){
+            char ch= s[i];
+            if(ch=='(' || ch=='{' || ch=='['){
+                str.push(ch);
+            }else{
+                if(str.empty()){
+                    return false;
+                }
+
+                int top= str.top();
+                if((top== '(' && ch== ')') ||
+                    (top== '[' && ch== ']') ||
+                    (top== '{' && ch== '}')){
+                        str.pop();
+                    }else{
+                        return false;
+                    }
+            }
+        }
+
+        return str.empty();
+    }
+};
